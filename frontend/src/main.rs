114df993a92c1be0d63db97f02677b775f08ec43
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-03-07 22:04:31 +0100

    progress on frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,26 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! {<p>"Timeline"</p>})
+    console_error_panic_hook::set_once();
+    mount_to_body(|| view! { <MainView/> })
+}
+
+#[component]
+fn MainView() -> impl IntoView {
+    view! { <TitleBar title="Hello" description=Some("Whaaazzz up".to_string())/> }
+}
+
+#[component]
+fn TitleBar(
+    #[prop(into)] title: MaybeSignal<String>,
+    #[prop(into, default=None.into())] description: MaybeSignal<Option<String>>,
+) -> impl IntoView {
+    view! {
+        <h1>{title}</h1>
+
+        {move || match description() {
+            Some(v) => view! { <b>{v}</b> }.into_view(),
+            None => view! {}.into_view(),
+        }}
+    }
 }
