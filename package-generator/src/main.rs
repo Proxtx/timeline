2e7c3e181577422c06bd2e97feebd1177aa6240b
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-05-02 23:59:15 +0200

    dependency manager working

--- package-generator/src/main.rs
+++ package-generator/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{fs::{File, read_dir, try_exists, write}, io::AsyncReadExt};
+
+#[tokio::main]
+async fn main() {
+    let mut file = File::open("main.Cargo.toml").await.expect("Did not find preset cargo file");
+    let mut str = String::new();
+    file.read_to_string(&mut str).await.expect("Unable to read preset cargo file to string");
+    let mut dirs = read_dir("../plugins/").await.expect("Unable to find plugins directory");
+    while let Some(entry) = dirs.next_entry().await.expect("Unable to read plugins directory") {
+        let mut new_path = entry.path().to_path_buf();
+        new_path.push("dependencies.txt");
+        if try_exists(&new_path).await.expect("Unable to check if dependencies file exists") {
+            let mut cont = String::new();
+            File::open(new_path).await.expect("Unable to read dependencies txt file").read_to_string(&mut cont).await.expect("Unable to parse dependencies txt file");
+            str.push_str(&format!("\n{}", cont));
+        }
+    }
+
+    write("../server/Cargo.toml", str).await.expect("Unable to write new Cargo.toml file");
 }
