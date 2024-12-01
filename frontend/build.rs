dbdc6689f1501a45b63f2b65927cfcfee405614d
Proxtx <gopro54122@gmail.com>
2024-12-01 15:39:11 +0100

    make the style generation work cross-platform

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,10 +1,17 @@
+#![feature(let_chains)]
+
 use {
     std::{env, path::PathBuf},
     stylers::build,
 };
 fn main() {
     println!("cargo:rerun-if-changed=src/");
-    let style_path =
-        PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("../../../../generated.css");
+    let mut style_path = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    while let Some(name) = style_path.file_name()
+        && name != "target"
+    {
+        style_path.pop();
+    }
+    style_path.push("generated.css");
     build(Some(style_path.display().to_string()));
 }
