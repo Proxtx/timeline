949984e1bd6b66445a0ca5365c641e79d33b1fbb
Proxtx <gopro54122@gmail.com>
2025-02-09 01:50:52 +0000

    it's not looking good

--- flake.nix
+++ flake.nix
@@ -1,109 +1,59 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url    = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    crane.url = "github:ipetkov/crane";
-    linker.url = "path:/home/nixos/timeline/linker/";
+    crane.url      = "github:ipetkov/crane";
+    linker.url     = "path:/home/nixos/timeline/linker/";
   };
 
-  outputs = {self, nixpkgs, flake-utils, crane, linker}:
-    flake-utils.lib.eachDefaultSystem(system:
-      let 
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, flake-utils, crane, linker }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs     = nixpkgs.legacyPackages.${system};
         craneLib = crane.mkLib pkgs;
       in {
-        packages.default = config: let 
+        packages.default = config: let
           plugins = config.plugins;
           experiences = builtins.fetchGit "https://github.com/Proxtx/experiences";
-          in 
-          craneLib.buildPackage {
-          src = craneLib.cleanCargoSource (let 
           plugin_repos = builtins.concatStringsSep "\n" (builtins.map (plugin: "cp -r ${(builtins.fetchGit plugin.url).outPath}/ $out/timeline/plugins/${plugin.name}") plugins);
-          cps = pkgs.runCommand "copy-source" { buildInputs = [ pkgs.git pkgs.cargo linker.packages.${system}.default ]; } ''
-                    mkdir -p $out
-                    cp -r ${./.} $out/timeline/
-                    chmod -R u+w $out
-                    mkdir $out/timeline/plugins
-                    cp -r ${experiences} $out/experiences
-                    echo -n "../experiences/" > $out/timeline/experiences_location.txt
-                    ${plugin_repos}
+          modifiedSource = pkgs.runCommand "modified-source" {
+            buildInputs = [
+              pkgs.git
+              pkgs.cargo
+              linker.packages.${system}.default
+            ];
+          } ''
+            # Create the base directory for the modified source.
+            mkdir -p $out
 
-                    cd $out/timeline/linker
-                    linker
-                  ''; in builtins.trace "${cps.outPath}/server" "${cps.outPath}/timeline/server");
+            # Copy the entire repository into $out.
+            cp -r ${./.} $out/timeline
+            rm -rf $out/git
+            chmod -R u+w $out
 
-          nativeBuildInputs = [ pkgs.git pkgs.cargo ];
+            # Clone the experiences repository into $out/experiences and record its location.
+            cp -r ${experiences} $out/experiences 
+            echo -n "../experiences/" > $out/timeline/experiences_location.txt
 
-          /*prePatch = ''
-            echo "Creating external dependencies folder..."
-            mkdir -p plugins
+            # Create the plugins directory and clone each plugin.
+            mkdir -p $out/timeline/plugins
+            ${plugin_repos}
 
-            echo "Cloning repositories..."
-            git clone  plugins/
-
-            echo "Running Rust tool to modify files..."
-            cd linker
-            cargo run .
-          '';*/
-
-        };
-      }) // {
-      /*nixosModules.default = {config, lib, pkgs, ...} : {
-        options.services.timeline = {
-          enable = pkgs.lib.mkEnableOption "Timeline";
-          config = lib.mkOption {
-            type = lib.types.path;
-            default = "";
-            description = "Configuration for timeline.";
-          };
-          cwd = lib.mkOption {
-            type = lib.types.path;
-            default = "/var/lib/timeline/";
-            description = "Sets the current working directory";
-          };
-          package = lib.mkOption {
-            type = lib.types.package;
-            default = self.packages.${pkgs.system}.default;
-            description = "Timeline";
-          };
-        };
-
-        config = lib.mkIf config.services.tiemline.enable {
-          
-          users.groups = {
-            timeline = {};
-          };
-
-          users.users = {
-            timeline = {
-              group = "timeline";
-              isSystemUser = true;
-            };
-          };
-          
-          system.activationScripts.copyConfigTimeline = ''
-            cp ${config.services.timeline.config} ${config.services.timeline.data_dir}/config.toml
+            # Run the linker command from within the linker folder.
+            cd $out/timeline/linker && linker
           '';
+        in craneLib.buildPackage {
+          # Use the preprocessed source as the input.
+          src = "${modifiedSource}/timeline/server";
+          # Provide the absolute path to the Cargo.toml file.
+          # cargoToml = "${modifiedSource}/timeline/server/Cargo.toml";
+          nativeBuildInputs = [
+            pkgs.git
+            pkgs.cargo
+            linker.packages.${system}.default
+          ];
 
-          systemd.services.timeline = {
-            wantedBy = ["multi-user.target"];
-            serviceConfig = {
-              ExecStart = "${config.services.timeline.package}/bin/timeline";
-              Restart = "always";
-              User = "timeline";
-              WorkingDirectory = "${config.services.timeline.data_dir}";
-              Group = "timeline";
-            };
-          };
-
-          systemd.tmpfiles.settings = {
-            "timelineStorage".${config.services.timeline.data_dir}.d = {
-              user = "timeline";
-              group = "timeline";
-              mode = "0770";
-            };
-          };
+          cargoVendorDir = null;
         };
-      };*/
-    };
+      });
 }
